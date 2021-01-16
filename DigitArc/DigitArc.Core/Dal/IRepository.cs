a912a60129517574eb69a86bad505ccae28d4c51
chatay <cagataygr@gmail.com>
2021-01-16 18:25:08 +0300

    IRepo and product data class has been added

---
+++ DigitArc/DigitArc.Core/Dal/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DigitArc.Core
+{
+    public interface IRepository<T> where T: class
+    {
+        IQueryable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        int Save();
+    }
+}
---
+++ DigitArc/DigitArc.ProductModule.Entities/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitArc.ProductModule.Entities.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name{ get; set; }
+        public byte[] Image { get; set; }
+        public decimal Price { get; set; }
+    }
+}
